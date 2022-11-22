401e44ae8586fd863addf9741bf5bdddb5e95165
drewros <arosenberg3@hawk.iit.edu>
2022-11-22 13:10:49 -0600

    Added markers

--- js/script.js
+++ js/script.js
@@ -3,10 +3,23 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.87321799627811, lng: -87.62801527359747},
-    zoom: 18
+    zoom: 17
   });
 
+  const jones = new google.maps.Marker({
+    position: {lat: 41.87321799627811, lng: -87.62801527359747},
+    map: map,
+  });
 
+  const potbelley = new google.maps.Marker({
+    position: {lat: 41.87515516090486, lng: -87.62951731727257},
+    map: map,
+  });
+
+  const harrison = new google.maps.Marker({
+    position: {lat: 41.87433712200045, lng: -87.62768714750437},
+    map: map,
+  });
 
 }
 
