54801300e7879c201637d74406b23ea0f6a25675
drewros <arosenberg3@hawk.iit.edu>
2022-11-22 10:14:30 -0600

    Changed var map

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+var map;
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
