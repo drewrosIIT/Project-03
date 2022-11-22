4ff3e2188c398d0209132b3854e1e8b11121befc
drewros <arosenberg3@hawk.iit.edu>
2022-11-22 11:36:03 -0600

    Changed JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function initMap(){
-var map = new google.maps.Map(document.getElementById('map'), {
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    zoom: 8,
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+window.initMap = initMap;
