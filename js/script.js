c4fa90b4737d426d12bd8d3bb991c7ddcf67143d
drewros <arosenberg3@hawk.iit.edu>
2022-11-22 13:37:11 -0600

    Added markers

--- js/script.js
+++ js/script.js
@@ -19,6 +19,21 @@ function initMap() {
   const harrison = new google.maps.Marker({
     position: {lat: 41.87433712200045, lng: -87.62768714750437},
     map: map,
+    title: "JCP",
+  });
+
+
+  const jonesWindow = new google.maps.InfoWindow({
+    content: "This is Jones College Prep, which is where I go to school.",
+    ariaLabel: "Jones College Prep",
+  });
+
+
+  jones.addListener("click", () => {
+    infowindow.open({
+      anchor: jones,
+      map: map,
+    });
   });
 
 }
